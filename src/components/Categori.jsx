18914165f529fd2b93e8f88bca673accd62cf645
stavbers <reddevweb@gmail.com>
2023-06-20 13:00:16 +0300

    some fix

--- src/components/Categori.jsx
+++ src/components/Categori.jsx
@@ -1,26 +1,31 @@
 function Categori(props) {
-  const {id, word, translate, exempleEn, exempleRu} = props
-  // console.log(id, word, translate, exempleEn, exempleRu)
-  console.log(props)
-  
-  return   (
-    <div id={id} className="row">
-      <div className="col">
-        <div className="card">
-          <div className="card-image">
-          </div>
-          <div className="card-content">
-            <p>{word}</p>
-            <p style={{color: 'red', fontSize: '22px'}}>{translate}</p>
-            <p>{exempleEn}</p>
-            <p>{exempleRu}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    const { id, word, translate, exempleEn, exempleRu } = props;
+ 
     
-  
-    )
+
+    return (
+        <div id={id} className='row'>
+            <div className='col'>
+                <div className='card'>
+                    <div className='card-image'></div>
+                    <div className='card-content'>
+                        <p>{word}</p>
+                        <p style={{ color: 'red', fontSize: '22px' }}>
+                            {translate}
+                        </p>
+                        {exempleEn.split('\r').map((el, ind) => (
+                            <p key={ind}>
+                            {ind + 1}{').'} {el}</p>
+                        ))}
+                        {exempleRu.split('\r').map((el, ind) => (
+                          <p key={ind}>
+                            {ind + 1}{').'} {el}</p>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export {Categori}
+export { Categori };
