9411831667b561a434c41237bf5dd2642775015c
stavbers <reddevweb@gmail.com>
2023-06-23 12:05:06 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Contact} from './pages/Contact'
+import {NotFound} from './pages/NotFound'
+import {SingleCategori} from './pages/SingleCategori'
 import './App.css';
 
 function App() {
 
   return (
-    <Home />
+    <>
+      <Router>
+        <main className='container'>
+          <Switch>
+              <Route exact path='/'>
+                <Home/>
+              </Route>
+              <Route path='/about' component={About}/>
+              <Route path='/contact' component={Contact}/>
+              <Route path='/categori/:name' component={SingleCategori}/>
+              <Route component={NotFound}/>
+          </Switch>
+        </main>
+      </Router>
+    </>
   );
 }
 
