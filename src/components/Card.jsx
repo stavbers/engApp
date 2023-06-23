8747d8c05d2938b45c6e0745dbdd32d7f4e1ac28
stavbers <reddevweb@gmail.com>
2023-06-23 16:30:02 +0300

    some fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,22 @@
 function Card(props) {
 const {id, word, translate, exempleEn, exempleRu} = props
-  
-return <h1 id={id}>{word}</h1>
+function generExaple(example) {
+  return example.split('\r').map((el, ind) => (
+      <p key={ind}>
+          {ind + 1}
+          {').'} {el}
+      </p>
+  ));
+}
+return (
+  <div id={id} className="simple-card">
+    <div className="simple-card__name">{word.toUpperCase()}</div>
+    <div className="simple-card__transl">{translate}</div>
+    <div className="simple-card__exemple">{generExaple(exempleEn)}</div>
+    <div className="simple-card__exemple simple-card__exemple_ru">{generExaple(exempleRu)}</div>
+  </div>
+)
 
 
-
-
-    // function generExaple(example) {
-    //     return example.split('\r').map((el, ind) => (
-    //         <p key={ind}>
-    //             {ind + 1}
-    //             {').'} {el}
-    //         </p>
-    //     ));
-    // }
 }
 export {Card}
