118054110e4aaf16ae267ac675a0886cb7160055
stavbers <reddevweb@gmail.com>
2023-06-30 00:29:35 +0300

    new styles too notFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,16 @@
+import { Link } from 'react-router-dom';
+
 function NotFound() {
-  return <h1>Page not found</h1>;
+    return (
+        
+            <div className='notFound'>
+                <div className='notFound-wrp'>
+                    <h1>404</h1>
+                    <h2>Page not found</h2>
+                    <Link to={'/'}> Go Home Page</Link>
+                </div>
+            </div>
+        
+    );
 }
 export { NotFound };
