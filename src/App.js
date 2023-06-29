09be43fab5fe13ee10a6d7a609240b3036b4e296
stavbers <reddevweb@gmail.com>
2023-06-30 01:38:30 +0300

    some fix

--- src/App.js
+++ src/App.js
@@ -1,39 +1,42 @@
-import React from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { deepOrange } from '@mui/material/colors';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import {Home} from './pages/Home'
-import {About} from './pages/About'
-import {Contact} from './pages/Contact'
-import {NotFound} from './pages/NotFound'
-import {SingleCategori} from './pages/SingleCategori'
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { NotFound } from './pages/NotFound';
+import { SingleCategori } from './pages/SingleCategori';
 import './App.css';
 
 function App() {
-  const AppTheme = createTheme({
-    palette: {
-      primary: {
-        main: deepOrange[500],
-      },
-    },
-  });
-  return (
-    <>
-      <Router>
-      <ThemeProvider theme={AppTheme}>
-          <Switch>
-              <Route exact path='/'>
-                <Home/>
-              </Route>
-              <Route path='/about' component={About}/>
-              <Route path='/contact' component={Contact}/>
-              <Route path='/categori/:name' component={SingleCategori}/>
-              <Route component={NotFound}/>
-          </Switch>
-        </ThemeProvider>
-      </Router>
-    </>
-  );
+    const AppTheme = createTheme({
+        palette: {
+            primary: {
+                main: deepOrange[500],
+            },
+        },
+    });
+    return (
+        <>
+            <Router>
+                <ThemeProvider theme={AppTheme}>
+                    <Switch>
+                        <Route exact path='/'>
+                            <Home />
+                        </Route>
+                        <Route path='/about' component={About} />
+                        <Route path='/contact' component={Contact} />
+                        <Route
+                            path='/categori/:name'
+                            component={SingleCategori}
+                        />
+                        <Route component={NotFound} />
+                    </Switch>
+                </ThemeProvider>
+            </Router>
+        </>
+    );
 }
 
 export default App;
--- src/components/CategoriList.jsx
+++ src/components/CategoriList.jsx
@@ -1,23 +1,16 @@
-import Box from '@mui/material/Box';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import { Categori } from './Categori';
 
 function CategoriList(props) {
     const { categori = [] } = props;
 
     return (
-        <Box sx={{ width: 1000, height: 450, overflowY: 'none' }}>
-            <ImageList variant='masonry' cols={3} gap={8}>
-                {categori.map((item) => (
-                    <ImageListItem key={item.img}>
-                        <div className='categoriList'>
-                        <Categori key={item.id} {...item} />
-                        </div>
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </Box>
+        <div className='categoriList'>
+            {categori.length ? (
+                categori.map((cat) => <Categori key={cat.id} {...cat} />)
+            ) : (
+                <h4>Nothing found</h4>
+            )}
+        </div>
     );
 }
 export { CategoriList };
