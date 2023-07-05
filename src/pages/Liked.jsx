6ec2898207c6768fdbed5dbc7566f2a0d03bd32a
stavbers <reddevweb@gmail.com>
2023-07-05 17:54:50 +0300

    add clear localstorga fn

--- src/pages/Liked.jsx
+++ src/pages/Liked.jsx
@@ -7,6 +7,8 @@ import { Preloader } from '../components/Preloader';
 function Liked() {
   const { goBack } = useHistory();
   const [items, setItems] = useState([]);
+  const [stor, setStor] = useState(false);
+
   useEffect(() => {
     const el = JSON.parse(localStorage.getItem('liked'));
     if (el) {
@@ -14,9 +16,22 @@ function Liked() {
     }
 }, []);
 
+useEffect(() => {
+    if(stor){
+        localStorage.removeItem("liked");
+        goBack()
+    }
+
+}, [stor])
+
 return (
     <>
+    <div className="wrp" style={{display: "flex", justifyContent: "space-between"}}>
     <button className='btn' onClick={goBack}>Back</button>
+    <button className='btn' onClick={()=> {setStor(prev => !prev)}}>Clear Storage</button>
+    </div>
+
+
         {!items.length ? <Preloader /> 
             : items.map((item, index) => (
                 <LikedCard key={index} {...item} />
